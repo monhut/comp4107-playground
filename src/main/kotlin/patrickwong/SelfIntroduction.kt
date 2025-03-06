23e1f8743e44064205b98ac6439c4cd602069cd8
Wong Cheuk Wing <23220783@life.hkbu.edu.hk>
2025-03-06 14:35:16 +0800

    Resolve merge conflicts

--- src/main/kotlin/patrickwong/SelfIntroduction.kt
+++ src/main/kotlin/patrickwong/SelfIntroduction.kt
@@ -2,7 +2,7 @@ package hk.edu.hkbu.comp.comp4107.playground.patrickwong
 
 class SelfIntroduction {
     fun selfIntroduction() {
-        println("Hello, my name is Kenny Cheng.")
+        println("Hello, my name is Patrick Wong.")
         println("I am a student at HKBU.")
         println("I am studying Computer Science.")
         println("I am interested in machine learning.")
