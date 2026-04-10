c9fb1a2fe757fa5afb9b2feace842dc838aa2731
monhut <cheukwinglol@gmail.com>
2026-04-10 22:48:21 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/wongcheukwing/SelfIntroduction.kt
@@ -0,0 +1,11 @@
+package hk.edu.hkbu.comp.comp4107.playground.wongcheukwing
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Wong Cheuk Wing.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
+
